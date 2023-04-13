80cc7838cf63991c556e4f4738d712d8b3f3c1c6
asheroh <wnstj701@naver.com>
2023-04-13 20:35:49 +0900

    Design: Add home page nav-bar container

--- Landing/landing.js
+++ Landing/landing.js
@@ -7,8 +7,12 @@ form?.addEventListener("submit", function (e) {
   if (name === localStorage.getItem("name")) {
     alert("이미 등록된 이름입니다. 해당 페이지로 이동합니다.")
     window.location.href = "../Main/home.html";
+  } else if (name === "") {
+    alert("이름을 입력해주세요.")
+    return;
   } else {
     alert("정상적으로 등록되었습니다.")
+    localStorage.removeItem("name")
     localStorage.setItem("name", name);
     window.location.href = "../Main/home.html";
   }
