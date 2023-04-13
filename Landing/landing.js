a075d7d166fee6b686653216298eb30b6b35594c
asheroh <wnstj701@naver.com>
2023-04-13 20:01:30 +0900

    Feat: Add fonts files

--- Landing/landing.js
+++ Landing/landing.js
@@ -4,8 +4,13 @@ const input = form.querySelector("#name-input");
 form?.addEventListener("submit", function (e) {
   e.preventDefault();
   const name = input.value;
-  localStorage.setItem("name", name);
-  window.location.href = "../Main/home.html";
-  console.log(name);
+  if (name === localStorage.getItem("name")) {
+    alert("이미 등록된 이름입니다. 해당 페이지로 이동합니다.")
+    window.location.href = "../Main/home.html";
+  } else {
+    alert("정상적으로 등록되었습니다.")
+    localStorage.setItem("name", name);
+    window.location.href = "../Main/home.html";
+  }
 });
 
