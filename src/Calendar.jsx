82489f70e1c156ba6a127d11443f063d1ef1111f
tnfus916 <tnfus1999@gmail.com>
2023-06-02 23:31:33 +0900

    Feat: Add Title and FullCalendar

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+import {
+  PageContainer,
+  CalendarSection,
+  Title,
+} from './components/CalendarStyle';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import './components/calendarStyle.css';
 
 
 const Calendar = () => {
+  
+  const userName = localStorage.getItem('username');
+
   return (
     <>
-      <p>Calendar 페이지</p>
+    <PageContainer>
+      <CalendarSection>
+        <Title>{userName}님의 가계부</Title>
+        <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+          events={[
+            { title: '웹프 기말고사', date: '2023-06-08' },
+            { title: '웹프 프로젝트 제출', date: '2023-06-15' }
+          ]}  
+          contentHeight={400}
+        />
+      </CalendarSection>
+    </PageContainer>
     </>
   );
 };
--- src/components/CalendarStyle.jsx
+++ src/components/CalendarStyle.jsx
@@ -1,1 +1,32 @@
 // Calendar 페이지와 관련된 styled component
+import styled from 'styled-components';
+import { primaryColor, bgColor } from '../../GlobalStyle';
+
+
+
+export const PageContainer = styled.div`
+  width: 100%;
+  height: 92vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const CalendarSection = styled.section`
+  width: 100%;
+  height: 100%;
+  padding:20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  top:30px;
+  z-index: 0;
+`;
+
+export const Title=styled.div`
+    font-size: 20px;
+    font-family: 'AppleSDGothicNeoB';
+    color: ${primaryColor};
+    margin-bottom: 10px;
+`;
