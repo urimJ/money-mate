b6e4831b30176c283c27e23b6954d03275488c8d
asheroh <wnstj701@naver.com>
2023-06-07 19:43:45 +0900

    Design: Modify guide page detail

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -7,8 +7,10 @@ import {
 } from './components/NotFoundStyle';
 import { faPersonWalkingArrowLoopLeft } from '@fortawesome/free-solid-svg-icons';
 import Header from './components/Layout/Header';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Header />
@@ -18,7 +20,9 @@ const NotFound = () => {
           <FontAwesomeIcon icon={faPersonWalkingArrowLoopLeft} />
         </IconBox>
         <WarningTextBox>잘못된 접근이에요</WarningTextBox>
-        <GoBackButton>돌아가기</GoBackButton>
+        <GoBackButton onClick={() => navigate('/', { replace: true })}>
+          돌아가기
+        </GoBackButton>
       </NotFoundContainer>
     </>
   );
