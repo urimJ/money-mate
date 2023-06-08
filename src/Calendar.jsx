b42d5343955274774347c96ae87600cb77ebc6e0
tnfus916 <tnfus1999@gmail.com>
2023-06-08 20:47:42 +0900

    Design: Modify calendar detail css

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -14,15 +14,34 @@ import './calendarStyle.css';
 import Modal from 'react-modal';
 import { TextField } from '@mui/material';
 import {useNavigate} from 'react-router-dom';
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 const Calendar = () => {
   
   const navigate = useNavigate();
   const userName = localStorage.getItem('username');
+  const schedule = JSON.parse(localStorage.getItem('schedule'));
+
+  function Schedule({schedule}){
+    return (
+      schedule.date==date? <div>{schedule.date} {schedule.title} </div> : <div></div>
+    );
+  }
   
-  const [isModalOpen,setIsModalOpen]=useState(false);
-  const openModal=()=>setIsModalOpen(true);
-  const closeModal=()=>setIsModalOpen(false);
+  const [isInputModalOpen,setIsInputModalOpen]=useState(false);
+  const openInputModal=()=>setIsInputModalOpen(true);
+  const closeInputModal=()=>setIsInputModalOpen(false);
+
+  const [isScheduleModalOpen,setIsScheduleModalOpen]=useState(false);
+  const openScheduleModal=()=>{
+    setIsScheduleModalOpen(true);
+    // setDate(e.target.value);  // ?? 날짜가 찍혀야할텐데 뭐가 찍힐까
+  }
+  const closeScheduleModal=()=>setIsScheduleModalOpen(false);
+
+  const [isModifyModalOpen,setIsModifyModalOpen]=useState(false);
+  const openModifyModal=()=>setIsModifyModalOpen(true);
+  const closeModifyModal=()=>setIsModifyModalOpen(false);
 
   const [content,setContent]=useState('');
   const [date,setDate]=useState('');
@@ -32,7 +51,7 @@ const Calendar = () => {
   ]);
 
   const dateChange=(e)=>{
-    setDate(e.target.value)
+    setDate(e.target.value);
   };
 
   const contentChange=(e)=>{
@@ -45,7 +64,7 @@ const Calendar = () => {
       ...list,
       {title:content,date:date}
     ])
-    closeModal();
+    closeInputModal();
     navigate('/calendar',{replace:true});
   }
 
@@ -53,21 +72,58 @@ const Calendar = () => {
     localStorage.setItem('schedule',JSON.stringify(list));
   },[list]);
 
+
   return (
     <>
     <PageContainer>
       <CalendarSection>
         <Title>{userName}님의 가계부</Title>
         <FullCalendar
-          plugins={[ dayGridPlugin ]}
+          plugins={[ dayGridPlugin, interactionPlugin ]}
+          // headerToolbar={{
+          //   left: 'prev,next today',
+          //   center: 'title',
+          //   right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          // }}
           initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
           events={list}
           contentHeight={400}
+          eventClick={openScheduleModal}
         />
-        <CalendarButton onClick={openModal}>일정추가</CalendarButton>
         <Modal 
-          isOpen={isModalOpen} 
-          onRequestClose={closeModal}
+          isOpen={isScheduleModalOpen} 
+          onRequestClose={closeScheduleModal}
+          style={ModalStyle}
+          ariaHideApp={false}
+          >
+          <h1>일정 리스트</h1>
+          <ModalForm>
+            <div>{schedule?.map((sche,idx)=>(<Schedule key={idx} schedule={sche}/>))}</div>
+            {/* 일정을 클릭하면 textfield와 수정, 삭제 버튼이 뜨도록 하기  */}
+            <ModalButton onClick={closeScheduleModal}>닫기</ModalButton>
+          </ModalForm>
+        </Modal>
+        <Modal>
+            <TextField
+              label="변경할 일정을 입력해주세요."
+              variant="outlined"
+              color="info"
+              type="schedule"
+              required
+              onChange={contentChange}
+              sx={{ width: '200px' }}
+              ></TextField>
+            <ModalButton type="submit">수정</ModalButton>
+            <ModalButton type="submit">삭제</ModalButton>
+            <ModalButton onClick={closeModifyModal}>닫기</ModalButton>
+        </Modal>
+        <CalendarButton onClick={openInputModal}>일정추가</CalendarButton>
+        <Modal 
+          isOpen={isInputModalOpen} 
+          onRequestClose={closeInputModal}
           style={ModalStyle}
           ariaHideApp={false}
           >
@@ -87,7 +143,7 @@ const Calendar = () => {
               sx={{ width: '200px' }}
               ></TextField>
             <ModalButton type="submit">추가</ModalButton>
-            <ModalButton onClick={closeModal}>닫기</ModalButton>
+            <ModalButton onClick={closeInputModal}>닫기</ModalButton>
           </ModalForm>
         </Modal>
       </CalendarSection>
